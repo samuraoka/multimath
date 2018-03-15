1f3464b56e24b2fbc73a7e4108e08c1f788ee30d
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-15 18:10:22 +0900

    Demo: Installing TypeScript and Running the compiler.

--- app/app.js
+++ app/app.js
@@ -3,5 +3,4 @@ function startGame() {
     var messageElement = document.getElementById('messages');
     messageElement.innerText = 'Welcome to MultiMath! Starting new game...';
 }
-
 document.getElementById('startGame').addEventListener('click', startGame);
